7a884982aa81023062a7978e4b7213399dde5085
BurakBebek1 <burakbebek1994@gmail.com>
2022-01-21 15:19:34 +0300

    Facade

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    internal class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    internal interface ILogging
+    {
+        void Log();
+    }
+
+    internal class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached");
+        }
+    }
+
+    internal interface ICaching
+    {
+        void Cache();
+    }
+
+    internal class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User Checked");
+        }
+    }
+
+    internal interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    internal class CustomerManager
+    {
+        private CrossCuttingConcernsFacade _conserns;
+
+        public CustomerManager()
+        {
+            _conserns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _conserns.Caching.Cache();
+            _conserns.Authorize.CheckUser();
+            _conserns.Logging.Log();
+            Console.WriteLine("Saved");
+        }
+    }
+
+    internal class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+        }
+    }
+}
