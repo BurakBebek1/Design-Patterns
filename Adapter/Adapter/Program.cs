37d9670c8c12d99d8e5a0c1adc12e292a259323c
BurakBebek1 <burakbebek1994@gmail.com>
2022-01-24 14:25:52 +0300

    Adapter

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new BbLogger());
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    internal class ProductManager
+    {
+        private ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User Data");
+            Console.WriteLine("Saved");
+        }
+    }
+
+    internal interface ILogger
+    {
+        void Log(string message);
+    }
+
+    internal class BbLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged, {0}", message);
+        }
+    }
+
+    internal class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged with log4net, {0}", message);
+        }
+    }
+
+    internal class Log4NetAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
