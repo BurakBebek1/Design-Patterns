d4c2455c744b2fe911cdd3b7fb6f1376664989dd
BurakBebek1 <burakbebek1994@gmail.com>
2022-01-26 16:02:46 +0300

    Chain Of Responsibility

---
+++ ChainOfResponsibility/ChainOfResponsibility/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            VicePresident vicePresident = new VicePresident();
+            President president = new President();
+
+            manager.SetSuccessor(vicePresident);
+            vicePresident.SetSuccessor(president);
+
+            Expense expense = new Expense { Detail = "Training", Amount = 900 };
+            manager.HandleExpense(expense);
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class Expense
+    {
+        public string Detail { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    internal abstract class ExpenseHandlerBase
+    {
+        protected ExpenseHandlerBase Successor;
+
+        public abstract void HandleExpense(Expense expense);
+
+        public void SetSuccessor(ExpenseHandlerBase successor)
+        {
+            Successor = successor;
+        }
+    }
+
+    internal class Manager : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount <= 100)
+            {
+                Console.WriteLine("Manager handled the expense!");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    internal class VicePresident : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 100 && expense.Amount <= 1000)
+            {
+                Console.WriteLine("Vice President handled the expense!");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    internal class President : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 1000)
+            {
+                Console.WriteLine("President handled the expense!");
+            }
+        }
+    }
+}
