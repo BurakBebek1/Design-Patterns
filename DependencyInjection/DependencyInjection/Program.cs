307b6368aac85b4a918a33007dcc9e89fb3024a5
BurakBebek1 <burakbebek1994@gmail.com>
2022-02-09 11:28:35 +0300

    Dependency Injection

---
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,58 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EFProductDal>().InSingletonScope();
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    internal interface IProductDal
+    {
+        void Save();
+    }
+
+    internal class EFProductDal : IProductDal//Entity Framework
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with EF");
+        }
+    }
+
+    internal class NHProductDal : IProductDal//NHibernate
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+    internal class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
